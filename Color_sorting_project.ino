61102148417369193bc5df2a1bb3515c5b830a0f
Syed Daniyal Ali <SyedDaniyalAli2222@gmail.com>
2022-03-13 21:13:07 +0500

    initial commit

--- Color_sorting_project.ino
+++ Color_sorting_project.ino
@@ -1,3 +1,4 @@
+#include <HX711.h>
 #include <LiquidCrystal_SR3W.h>
 #include <LiquidCrystal_I2C.h>
 #include <FastIO.h>
@@ -7,17 +8,18 @@
 #include <LiquidCrystal_SR2W.h>
 #include <LiquidCrystal_SR.h>
 
+
 #include <LiquidCrystal_I2C.h>
 
 #include <LiquidCrystal_I2C.h>
 
 /*     Arduino Project - Color Sorting Machine
- *
- *  developed by Syed Daniyal Ali  #SDA
- *
- */
 
- #include <Wire.h>
+    developed by Syed Daniyal Ali  #SDA
+
+*/
+
+#include <Wire.h>
 
 
 /*-----( Inport library )-----*/
@@ -31,24 +33,37 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE); //
 #define S2 4
 #define S3 5
 #define sensorOut 6
+#define calibration_factor -7050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+
+#define LOADCELL_DOUT_PIN  9
+#define LOADCELL_SCK_PIN  10
+
+HX711 scale;
+
+
+
 const int analogInPin = A3; //LDR Pin
 Servo topServo;
 Servo bottomServo;
 int frequency = 0;
-int color=0;
-int buttonState = 0; 
-int OrangeCount = 0;
-int PurpleCount = 0;
+int color = 0;
+int buttonState = 0;
+int RedCount = 0;
+int RedHeavyCount = 0;
 int YellowCount = 0;
-int BlackCount = 0;
+int Unidentified = 0;
 int R = frequency;
 int G = frequency;
 int B = frequency;
+
+float current_weight = 0;
+float total_weight = 0;
+
 void setup() {
- //WE define our LCD 16 columns and 2 rows
-lcd.begin(20,4);
-lcd.backlight();//Power on the back light
-//lcd.backlight(); Power off the back light
+  //WE define our LCD 16 columns and 2 rows
+  lcd.begin(20, 4);
+  lcd.backlight();//Power on the back light
+  //lcd.backlight(); Power off the back light
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
@@ -61,166 +76,196 @@ lcd.backlight();//Power on the back light
   digitalWrite(S0, HIGH);
   digitalWrite(S1, LOW);
   digitalWrite(A1, HIGH);
-  
+
   topServo.attach(7);
+  topServo.write(180);
   bottomServo.attach(8);
+  bottomServo.write(30);
   Serial.begin(9600);
 
-lcd.clear();//Clean the screen
-//Write your text:
-//lcd.setCursor(0,1); //we start writing from the first row first column
-//lcd.print("      WELCOME"); //16 characters poer line
-//delay(3000);//Delay used to give a dinamic effect
-//lcd.clear();//Clean the screen
-//lcd.setCursor(0,1); //we start writing from the first row first column
-//lcd.print("Color Sorting System"); //16 characters poer line
-//delay(4000);//Delay used to give a dinamic effect
-//lcd.clear();//Clean the screen
-//
-//for(int i =0 ;i<5 ; i++)
-//{
-//lcd.setCursor(0,0);
-//lcd.print(" Loading");
-//delay(120);
-//lcd.setCursor(0,0);
-//lcd.print(" Loading.");
-//delay(120);
-//lcd.setCursor(0,0);
-//lcd.print(" Loading..");
-//delay(120);
-//lcd.setCursor(0,0);
-//lcd.print(" Loading...");
-//delay(120);
-//lcd.setCursor(0,0);
-//lcd.print(" Loading....");
-//delay(120);
-//lcd.setCursor(0,0);
-//lcd.print(" Loading.....");
-//delay(120);
-//lcd.setCursor(0,0);
-//lcd.print(" Loading......");
-//delay(120);
-//lcd.clear();
-//}
-//lcd.setCursor(3,1);
-//lcd.print("System Ready");
-//delay(120);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare(); //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+  lcd.clear();//Clean the screen
+  //  Write your text/:
+  lcd.setCursor(6, 1); //we start writing from the first row first column
+  lcd.print("WELCOME"); //16 characters poer line
+  delay(3000);//Delay used to give a dinamic effect
+  lcd.clear();//Clean the screen
+  lcd.setCursor(0, 1); //we start writing from the first row first column
+  lcd.print("Color Sorting System"); //16 characters poer line
+  delay(4000);//Delay used to give a dinamic effect
+  lcd.clear();//Clean the screen
+
+  for (int i = 0 ; i < 5 ; i++)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print(" Loading");
+    delay(120);
+    lcd.setCursor(0, 0);
+    lcd.print(" Loading.");
+    delay(120);
+    lcd.setCursor(0, 0);
+    lcd.print(" Loading..");
+    delay(120);
+    lcd.setCursor(0, 0);
+    lcd.print(" Loading...");
+    delay(120);
+    lcd.setCursor(0, 0);
+    lcd.print(" Loading....");
+    delay(120);
+    lcd.setCursor(0, 0);
+    lcd.print(" Loading.....");
+    delay(120);
+    lcd.setCursor(0, 0);
+    lcd.print(" Loading......");
+    delay(120);
+    lcd.clear();
+  }
+  lcd.setCursor(3, 1);
+  lcd.print("System Ready");
+  delay(120);
 
 }
 void loop() {
-int Ldr = analogRead(analogInPin);
-buttonState = digitalRead(A0);
-int Resetbutton = digitalRead(A2);
-if(buttonState==HIGH & Ldr<400){
-  
-  topServo.write(85);
-  delay(500);
-Ldr = analogRead(analogInPin);
-delay(10);
-  if(Ldr<400){
-  for(int i = 85; i > 48; i--) {
-    topServo.write(i);
-    delay(10);
-  }
-  delay(500);
-  
-  color = readColor();
-  delay(10);  
-  switch (color) {
-    case 1:
-    bottomServo.write(32);
-    break;
-    case 2:
-    bottomServo.write(60);
-    break;
-    case 3:
-    bottomServo.write(88);
-    break;
-    case 4:
-    bottomServo.write(120);
-   
-  }
-  delay(300);
-  
-  for(int i = 48; i > 12; i--) {
-    topServo.write(i);
-    delay(2);
-    
-  } 
-  lcd.clear();//Clean the screen
-    lcd.setCursor(0,0); 
-    lcd.print("Ora:");
-    lcd.print(OrangeCount);
-    lcd.setCursor(0,1); 
-    lcd.print("Pur:");
-    lcd.print(PurpleCount);
-    lcd.setCursor(9,0); 
-    lcd.print("Yel:");
-    lcd.print(YellowCount);
-    lcd.setCursor(9,1); 
-    lcd.print("Bla:");
-    lcd.print(BlackCount);
-  delay(200);
-  
-  for(int i = 12; i < 85; i++) {
-    topServo.write(i);
-    delay(2);
-  }
-  }
-  color=0;
+  current_weight = 0;
+  int Ldr = analogRead(analogInPin);
+  buttonState = digitalRead(A0);
+  int Resetbutton = digitalRead(A2);
+  if (buttonState == HIGH & Ldr < 400) {
 
-}
-if(buttonState==LOW )
-{
-  lcd.clear();//Clean the screen
-    lcd.setCursor(7,1); //we start writing from the first row first column
-    lcd.print("(Stop)"); 
+    topServo.write(180);
+    delay(500);
+    Ldr = analogRead(analogInPin);
+    delay(10);
+    if (Ldr < 400) {
+      //    move servo from 180 to 0~~~~~~~~~~~~~~~~~~~~~~
+      for (int i = 180; i > 65; i--) {
+        topServo.write(i);
+        delay(50);
+      }
+
+      current_weight = scale.get_units() * -453.59237; // Reading Weight
+
+      delay(1000);
+
+      Serial.print("\n current weight:");
+      Serial.print(current_weight, 1); //scale.get_units() returns a float
+      total_weight += current_weight;
+
+      color = readColor(); // Reading Color
+
+      delay(500);
+      switch (color) {
+        case 1:
+          bottomServo.write(30);
+          break;
+        case 2:
+          bottomServo.write(70);
+          break;
+        case 3:
+          bottomServo.write(118);
+          break;
+        case 4:
+          bottomServo.write(150);
+
+      }
+      delay(300);
+
+      for (int i = 65; i > 0; i--) {
+        topServo.write(i);
+        delay(50);
+
+      }
+      lcd.clear();//Clean the screen
+      lcd.setCursor(0, 0);
+      lcd.print("Red:");
+      lcd.print(RedCount);
+
+      lcd.setCursor(0, 1);
+      lcd.print("Yell:");
+      lcd.print(YellowCount);
+
+      lcd.setCursor(9, 0);
+      lcd.print("ReH:");
+      lcd.print(RedHeavyCount);
+
+      lcd.setCursor(9, 1);
+      lcd.print("UnI:");
+      lcd.print(Unidentified);
+
+      lcd.setCursor(0, 3);
+      lcd.print("CWt:");
+      lcd.print(current_weight, 0);
+      lcd.print("g ");
+
+      lcd.print("TWt:");
+      lcd.print(total_weight, 0);
+      lcd.print("g");
+
+
+      delay(2000);
+
+      for (int i = 0; i < 180; i++) {
+        topServo.write(i);
+        delay(2);
+      }
+    }
+    color = 0;
+
+  }
+  if (buttonState == LOW )
+  {
+    lcd.clear();//Clean the screen
+    lcd.setCursor(7, 1); //we start writing from the first row first column
+    lcd.print("(Stop)");
     delay(20);
     lcd.clear();
-}
-if(Resetbutton==HIGH)
-{
-OrangeCount = 0;
-PurpleCount = 0;
-YellowCount = 0;
-BlackCount = 0;
-   for(int i =0 ;i<5 ; i++)
-{
-  lcd.clear();
-lcd.setCursor(0,0);
-lcd.print("Resetting.");
-delay(120);
-lcd.setCursor(0,0);
-lcd.print("Resetting..");
-delay(120);
-lcd.setCursor(0,0);
-lcd.print("Resetting...");
-delay(120);
-lcd.setCursor(0,0);
-lcd.print("Resetting....");
-delay(120);
-lcd.setCursor(0,0);
-lcd.print("Resetting.....");
-delay(120);
-lcd.clear();
+  }
+  if (Resetbutton == HIGH)
+  {
+    RedCount = 0;
+    YellowCount = 0;
+    RedHeavyCount = 0;
+    for (int i = 0 ; i < 5 ; i++)
+    {
+      lcd.clear();
+      lcd.setCursor(2, 1);
+      lcd.print("Resetting.");
+      delay(120);
+      lcd.setCursor(2, 1);
+      lcd.print("Resetting..");
+      delay(120);
+      lcd.setCursor(2, 1);
+      lcd.print("Resetting...");
+      delay(120);
+      lcd.setCursor(2, 1);
+      lcd.print("Resetting....");
+      delay(120);
+      lcd.setCursor(2, 1);
+      lcd.print("Resetting.....");
+      delay(120);
+      lcd.clear();
+
+    }
+    lcd.setCursor(6, 1);
+    lcd.print("(Ready)");
+    delay(1000);
+
+  }
+  if (buttonState == HIGH & Ldr > 400)
+  {
+    lcd.clear();
+    lcd.setCursor(2, 1);
+    lcd.print("No Object Detect");
+    delay(2000);
+    lcd.clear();
+  }
 
 }
-lcd.setCursor(0,0);
-lcd.print("     Ready");
-delay(1000);
-    
-}
-if (buttonState==HIGH & Ldr>400)
-{
-lcd.clear();
-lcd.setCursor(0,1);
-lcd.print("No Object Detect");
-delay(20);
-lcd.clear();
-}
 
-}
-// Custom Function - readColor()
+// Custom Function - readColor()~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 int readColor() {
   // Setting red filtered photodiodes to be read
   digitalWrite(S2, LOW);
@@ -244,7 +289,7 @@ int readColor() {
   Serial.print(frequency);
   Serial.print("  ");
   delay(50);
-  
+
   digitalWrite(S2, LOW);
   digitalWrite(S3, HIGH);
   // Reading the output frequency
@@ -255,61 +300,59 @@ int readColor() {
   Serial.print(frequency);
   Serial.println("  ");
   delay(50);
-  
-  
-  if(R<62 & R>22 & G<113 & G>63 & B<93 & B>53){
-    
+
+
+  //  Checking RGB values~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  //  For RED~~~~~~~~~~~~~~~~~~~~~~~
+  if (R<90 & R>70 & G<280 & G>200) {
+
     lcd.clear();//Clean the screen
-    lcd.setCursor(0,0); 
-    lcd.print("     Orange");
+    Serial.print(current_weight);
+    if (current_weight > 250) {
 
-    color = 1; // R= 31  G= 71  B= 60  
-    
-    OrangeCount = OrangeCount+1;
-    
-    lcd.setCursor(8,1); 
-    lcd.print(OrangeCount); 
-  }
-  if(R<84 & R>44 & G<100 & G>50 & B<64 & B>24){
-     
-    lcd.clear();//Clean the screen
-    lcd.setCursor(0,0); 
-    lcd.print("    Purple"); 
+      lcd.setCursor(1, 1);
+      lcd.print("Heavy Red: ");
 
-    color = 2; // R= 55  G= 53  B= 51 
+      RedHeavyCount += 1;
+      lcd.print(RedHeavyCount);
+      color = 3;
+    }
+    else {
+      lcd.setCursor(3, 1);
+      lcd.print("Red: ");
 
-   PurpleCount = PurpleCount+1;
-    
-    lcd.setCursor(8,1); 
-    lcd.print(PurpleCount); 
-  }
-  if(R<53 & R>13 & G<69 & G>29 & B<88 & B>48){
-    
-    lcd.clear();//Clean the screen
-    lcd.setCursor(0,0); 
-    lcd.print("     Yellow"); 
+      RedCount += 1;
+      lcd.print(RedCount);
 
-    color = 3; // Yellow R= 33  G= 49  B= 68 
+      color = 1;
+    }
 
-    YellowCount = YellowCount+1;
-    
-    lcd.setCursor(8,1); 
-    lcd.print(YellowCount); 
-  }
-  if(R<110 & R>66 & G<130 & G>80 & B<105 & B>60 ){
-    
-    lcd.clear();//Clean the screen
-    lcd.setCursor(0,0); 
-    lcd.print("     Black"); 
+  } else
+    //  For YELLOW~~~~~~~~~~~~~~~~~~~~~~~
+    if (R<70 & R>35 & G<85 & G>45) {
 
-    color = 4; //  R= 69  G= 85  B= 67  
-    BlackCount = BlackCount+1;
-    
-    lcd.setCursor(8,1); 
-    lcd.print(BlackCount); 
-  }
+      lcd.clear();//Clean the screen
+      lcd.setCursor(1, 1);
+      lcd.print("Yellow: ");
 
- 
+      color = 2;
 
-  return color;  
+      YellowCount += 1;
+      lcd.print(YellowCount);
+    }
+
+  //  For Heavy RED~~~~~~~~~~~~~~~~~~~~~~~
+    else {
+
+      lcd.setCursor(1, 1);
+      lcd.print("Unidentified: ");
+      lcd.print(Unidentified);
+      Unidentified += 1;
+      color = 4;
+    }
+
+
+
+  return color;
 }
